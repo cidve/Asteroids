21b1e258e256593d3095d4aabd6725e0d236e3ad
cidve <cidveragabriel@gmail.com>
2024-10-16 15:12:25 -0300

    se crea variable reloj y setea FPS a 60

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,7 +15,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(0)
+        screen.fill("black")
         pygame.display.flip()
+
+        # limits the frame rate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 if __name__ == "__main__":
     main()
